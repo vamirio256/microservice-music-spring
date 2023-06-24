13b5a9b7ebf79f703e8e80e8a580613483706095
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2023-06-24 22:03:44 +0700

    Deleted backend folder

--- backend/audio-service/src/main/java/com/soundcloudclone/audioservice/repository/AudioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soundcloudclone.audioservice.repository;
-
-import com.soundcloudclone.audioservice.entity.Audio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AudioRepository extends JpaRepository<Audio,Long> {
-}
