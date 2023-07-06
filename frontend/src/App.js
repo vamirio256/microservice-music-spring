74903cf6540efa2b052f59bfe5d879ae9249dc73
nguyennhulonggithub <80632602+nguyennhulonggithub@users.noreply.github.com>
2023-07-06 21:22:41 +0700

    new login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import Login from "./components/Login";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="flex align-middle justify-center">
-      <HomePage />
+    <div className="flex justify-center">
+      <div className="container bg-orange-400 ma">
+        <HomePage />
+      </div>
+      <div className="w-screen flex justify-center items-center h-screen absolute bg-slate-200/50">
+        <Login />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className="w-[400px] bg-red-300 h-[500px] py-10 px-5">
+      <input type="text" className="px-1 py-1 w-full" />
+    </div>
+  );
+}
+
+export default Login;
