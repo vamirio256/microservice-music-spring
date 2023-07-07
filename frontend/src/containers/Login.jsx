3b5bdb1759556be6f2bfe957276f4155abb2a37d
nguyennhulonggithub <80632602+nguyennhulonggithub@users.noreply.github.com>
2023-07-07 16:34:06 +0700

    model login

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -30,41 +30,34 @@ const Login = ({ toggleModal }) => {
 
   return (
     <div>
-      <div className="w-screen h-screen fixed">
-        <div className="bg-[hsla(0,0%,94.9%,.9)]">
-          <div className="absolute top-[40%] left-[50%] transition max-h-[400px] max-w-[500px] ">
-            <form>
-              <button className={`${style} text-white bg-[#3578e5]`}>
-                Continue with Facebook
-              </button>
-              <button className={`${style} text-black border-[1px] border-[#e5e5e5]`}>
-                Continue with Google
-              </button>
-              <div className={`${style} `}>
-                
-                <span>or</span>::after
-              </div>
-              <input
-                type="text"
-                placeholder="Email"
-                value={email}
-                className={`${style} p-3 border-black border-[1px] focus:outline-none`}
-                onChange={(e) => setEmail(e.target.value)}
-              ></input>
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                className={`${style} p-3 border-[1px] border-[#e5e5e5] focus:outline-none`}
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-              <button className={`${style} text-white bg-[#f50]`} type="submit">
-                Submit
-              </button>
-            </form>
-          </div>
+      <form>
+        <button className={`${style} text-white bg-[#3578e5]`}>
+          Continue with Facebook
+        </button>
+        <button className={`${style} text-black border-[1px] border-[#e5e5e5]`}>
+          Continue with Google
+        </button>
+        <div className={`${style} `}>
+          <span>or</span>::after
         </div>
-      </div>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          className={`${style} p-3 border-black border-[1px] focus:outline-none`}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          className={`${style} p-3 border-[1px] border-[#e5e5e5] focus:outline-none`}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button className={`${style} text-white bg-[#f50]`} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- frontend/src/layouts/TopBar.jsx
+++ frontend/src/layouts/TopBar.jsx
@@ -4,9 +4,18 @@ import { BiLogoSoundcloud, BiSearch, BiSolidSearch } from "react-icons/bi";
 import TopBarItem from "../components/TopBarItem";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-const TopBar = () => {
-  const [loginModal, setLoginModal] = useState(false);
 
+import Modal from "react-modal";
+import Login from "../containers/Login";
+const TopBar = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const pathname = useLocation().pathname;
 
   const routes = useMemo(() => [
@@ -28,32 +37,67 @@ const TopBar = () => {
     },
   ]);
   const hr = "w-[0.4px] bg-black h-full";
+
   return (
-    <div className="flex w-full justify-content bg-[#333]">
-      <div className={`container bg-red h-12 flex m-auto items-center`}>
-        {routes.map((ele, index) => {
-          return (
-            <TopBarItem
-              key={index}
-              label={ele.label}
-              icon={ele.icon}
-              href={ele.href}
-              active={ele.active}
-            />
-          );
-        })}
-        {/* search */}
-        <input
-          type="text"
-          placeholder="Search"
-          className="px-2 py-1 w-96 ml-5 h-8 rounded-md focus:outline-none"
-        />
-        <AiOutlineSearch className="right-6 relative" color="gray" size={20} />
-        <button className="text-white rounded-sm border-solid border-[0.5px] border-slate-200 p-1 border-w ml-auto">
-          Sign in
-        </button>
+    <>
+      <div className="flex w-full justify-content bg-[#333]">
+        <div className={`container bg-red h-12 flex m-auto items-center`}>
+          {routes.map((ele, index) => {
+            return (
+              <TopBarItem
+                key={index}
+                label={ele.label}
+                icon={ele.icon}
+                href={ele.href}
+                active={ele.active}
+              />
+            );
+          })}
+          {/* search */}
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-2 py-1 w-96 ml-5 h-8 rounded-md focus:outline-none"
+          />
+          <AiOutlineSearch
+            className="right-6 relative"
+            color="gray"
+            size={20}
+          />
+
+          <button
+            className="text-white rounded-sm border-solid border-[0.5px] border-slate-200 p-1 border-w ml-auto"
+            onClick={openModal}
+          >
+            Sign in
+          </button>
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            style={{
+              overlay: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+              content: {
+                inset: "0",
+                position: "relative",
+                width: "450px",
+                height: "500px",
+              },
+            }}
+          >
+            <button onClick={closeModal} className="ml-auto mr-0 block">
+              x
+            </button>
+            <Login />
+          </Modal>
+        </div>
       </div>
-    </div>
+      {/* {loginModal && <Login />}x */}
+    </>
   );
 };
 
