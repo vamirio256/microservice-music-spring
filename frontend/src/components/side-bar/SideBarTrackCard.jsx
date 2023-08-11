6416c5091a1f9664ceed5b543871a20f19f02227
vamirio256 <nguyenquocquan.nbk.9a5@gmail.com>
2023-08-11 17:42:07 +0700

    Fix bug

--- frontend/src/components/side-bar/SideBarTrackCard.jsx
+++ frontend/src/components/side-bar/SideBarTrackCard.jsx
@@ -33,75 +33,84 @@ export const SideBarTrackCard = ({ track }) => {
   }
 
   useEffect(() => {
-    if (!playing) {
-      return;
-    }
+    if (!playing || playing === undefined) return;
     if (playing.track.id === track.id && playing.isPlaying === true)
       setIsPlaying(true);
     else setIsPlaying(false);
-  }, [playing]);
+  }, [playing.track, playing.isPlaying]);
 
   return (
-    <div className="flex mb-4 relative group w-full">
-      {/* image right */}
-      <div className="cursor-pointer w-[50px] relative group mt-1">
-        <img
-          src={track.coverUrl}
-          className="object-cover min-w-[50px] h-[50px] border-[0.5px] border-[#ccc]"
-        />
-        {/* play button */}
-        {isPlaying ? (
-          <BsFillPauseFill
-            size={20}
-            className="text-primary absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 hidden group-hover:block"
-            onClick={playTrack}
+    <>
+      <div className="flex mb-4 relative group w-full">
+        {/* image right */}
+        <div className="cursor-pointer w-[50px] relative group mt-1">
+          <img
+            src={track.coverUrl}
+            className="object-cover min-w-[50px] h-[50px] border-[0.5px] border-[#ccc]"
           />
-        ) : (
-          <FaPlay
-            size={20}
-            className="text-primary absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 hidden group-hover:block"
-            onClick={playTrack}
-          />
-        )}
-      </div>
+          {/* play button */}
+          {isPlaying ? (
+            <BsFillPauseFill
+              size={20}
+              className="text-primary absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 hidden group-hover:block"
+              onClick={playTrack}
+            />
+          ) : (
+            <FaPlay
+              size={20}
+              className="text-primary absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 hidden group-hover:block"
+              onClick={playTrack}
+            />
+          )}
+        </div>
 
-      {/* right information */}
-      <div className="pl-2 w-full">
-        <UserHoverBar user={track.user} />
-        <Link
-          to={`/track/${track.id}`}
-          className={`text-sm line-clamp-1 mt-[-2px] ${
-            isPlaying ? "text-[#f50]" : "text-black"
-          }`}
-        >
-          {track.name}
-        </Link>
-        {/* icon infor */}
-        <div className="grid grid-cols-3 gap-3 items-center text-xs mt-[2px] w-fit">
-          <div className="grid grid-cols-2 gap-0.5 items-center ">
-            <IoIosPlay color="#999" />
-            <p>{track.listenedTime}</p>
-          </div>
-          <div className="grid grid-cols-2 gap-0.5 items-center">
-            <GoHeartFill color="#999" className="text-[10px]" />
-            <p>11</p>
-          </div>
-          <div className="grid grid-cols-2 gap-0.5 items-center">
-            <BiSolidComment color="#999" className="text-[10px]" />
-            <p>12</p>
+        {/* right information */}
+        <div className="pl-2 w-full">
+          {track.user && (
+            <Link
+              to={`/user/${track.user.id}`}
+              className={`text-[11px] line-clamp-1 ${
+                isPlaying ? "text-[#f50]" : "text-[#999]"
+              }`}
+            >
+              {track.user.username}
+            </Link>
+          )}
+          <Link
+            to={`/track/${track.id}`}
+            className={`text-[13px] line-clamp-1 mt-[-2px] ${
+              isPlaying ? "text-[#f50]" : "text-black"
+            }`}
+          >
+            {track.name}
+          </Link>
+          {/* icon infor */}
+          <div className="grid grid-cols-3 gap-3 items-center text-xs mt-[2px] w-fit">
+            <div className="grid grid-cols-2 gap-0.5 items-center ">
+              <IoIosPlay color="#999" />
+              <p>{track.listenedTime}</p>
+            </div>
+            <div className="grid grid-cols-2 gap-0.5 items-center">
+              <GoHeartFill color="#999" className="text-[10px]" />
+              <p>11</p>
+            </div>
+            <div className="grid grid-cols-2 gap-0.5 items-center">
+              <BiSolidComment color="#999" className="text-[10px]" />
+              <p>12</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* hover button */}
-      <div
-        className={`
+        {/* hover button */}
+        <div
+          className={`
         ${isPlaying ? "visible" : "invisible group-hover:visible"}
         flex flex-row items-center justify-end text-black`}
-      >
-        <FavoriteButton track={track} haveBorder={true} className={"mr-2"} />
-        <MoreButton haveBorder={true} />
+        >
+          <FavoriteButton track={track} haveBorder={true} className={"mr-2"} />
+          <MoreButton haveBorder={true} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
